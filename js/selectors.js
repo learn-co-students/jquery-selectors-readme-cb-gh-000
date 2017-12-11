a35bcac78095a1520bebece74f9e79d63d8f2609
Victor Alagwu <victoralagwu@gmail.com>
2017-12-11 08:47:19 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
 // declare your functions here...
+function paragraphSelector () {
+  return $("p");
+}
+
+function lastImageSelector (){
+  return $("img:last");
+}
+//The function should use an ID selector to return the ninja baby image.
+function ninjaBabySelector() {
+  return $("#baby-ninja");
+}
+
+function divSelector(){
+  return $("div[class ='pics']");
+}
+function firstListItem(){
+  return $("ul li:first-child")
+}
