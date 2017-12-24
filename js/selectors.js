d494de16e18d8767b368788e60adfec17e302469
Mohamed Benkedadra <hammicristo@gmail.com>
2017-12-24 00:14:28 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,28 @@
 // declare your functions here...
+var paragraphSelector = function () {
+  var ps = $('p')
+  return ps
+}
+
+
+var lastImageSelector  = function () {
+  var imgsLast = $('img:last')
+  return imgsLast
+}
+
+
+var ninjaBabySelector = function () {
+  var ps = $('#baby-ninja')
+  return ps
+}
+
+
+var firstListItem  = function () {
+  var ps = $('ul#pic-list li:first-child')
+  return ps
+}
+
+var divSelector = function () {
+  var ps = $('div.pics')
+  return ps
+}
