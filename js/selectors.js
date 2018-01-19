488d883c486cfb8aa34cb18f5184995156ff2bf7
edwin rivera <waehake@gmail.com>
2018-01-19 15:29:56 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  var p = $('body p');
+  return p;
+}
+
+function lastImageSelector() {
+  var img = $('img:last');
+  return img;
+}
+
+function ninjaBabySelector() {
+  var babyninja = $('#baby-ninja');
+  return babyninja;
+}
+
+function divSelector() {
+  var pics = $('.pics');
+  return pics;
+}
+
+function firstListItem() {
+  var firstli = $('#pic-list li:first-child');
+  return firstli;
+}
