eb3aa936443d1c0ee5c7f7cf4d0c9210a6f5eec6
Logan Stone <logan.stone20@gmail.com>
2019-08-22 03:47:56 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $("p");
+}
+
+function lastImageSelector() {
+  return  $("img:last");
+}
+
+function ninjaBabySelector() {
+  return $("#baby-ninja");
+}
+
+function firstListItem() {
+  return $("li:first");
+}
+
+function divSelector() {
+  return $("div.pics");
+}
